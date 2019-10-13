0526a1dbf5f31aa36e56a9d0a0664c003b6f0ca5
Tirarex <tirarex98@gmail.com>
2019-10-13 18:41:06 +0400

    Add files via upload

---
+++ Radialmenu/Assets/RadialMenu/Scripts/RadialMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialMenu : MonoBehaviour
+{
+    public GameObject[] RadialMenuItems;
+    public AudioSource AudSourc;
+    public AudioClip MovementClip;
+    public AudioClip UseClip;
+    Vector2 NormalizedMPose;
+    float AngleOfMouse;
+    int SelectedItem;
+
+    int PrevItemID=5;
+    void Update()
+    {
+        NormalizedMPose = new Vector2(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2);
+        AngleOfMouse = Mathf.Atan2(NormalizedMPose.y, NormalizedMPose.x) * Mathf.Rad2Deg;
+        AngleOfMouse = (AngleOfMouse + 360)%360;
+        SelectedItem =(int) AngleOfMouse / 45;
+
+        if (PrevItemID != SelectedItem)
+        {
+            AudSourc.PlayOneShot(MovementClip);
+            RadialMenuItems[SelectedItem].GetComponent<RadialMenuItem>().SetItemState(true);
+            RadialMenuItems[PrevItemID].GetComponent<RadialMenuItem>().SetItemState(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)|| Input.GetMouseButtonDown(0))
+        {
+            AudSourc.PlayOneShot(UseClip);
+            RadialMenuItems[SelectedItem].GetComponent<RadialMenuItem>().OnUse();
+        }
+
+        PrevItemID = SelectedItem;
+     }
+}
---
+++ Radialmenu/Assets/RadialMenu/Scripts/RadialMenuItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RadialMenuItem : MonoBehaviour
+{
+    public Image ItemBG;
+    public Image ItemPic;
+    public Sprite BgUnSelected;
+    public Sprite BgSelected;
+
+    public void OnUse()
+    {
+        Debug.Log("I'm used");
+    }
+
+    public void SetItemState(bool ItemState)
+    {
+        if (ItemState)
+        {
+            ItemBG.sprite = BgSelected;
+        }
+        else
+        {
+            ItemBG.sprite = BgUnSelected;
+        }
+    }
+}
